df5e3b9f991fc78535a93682141f62dfccb962d5
YotamBG <yotam.bengera@gmail.com>
2022-05-13 16:48:31 +0300

    first commit

---
+++ resources/js/script.js
@@ -0,0 +1,8 @@
+/*
+Back-end: generate random jokes using nouns, adjs and verbs
+Front-end: ?
+GitHub: shared repo
+timeline: ?
+README doc: Title, Description, Features, How to use, Technologies, Collaborators, License
+*/
+console.log('HI!');
